bd09dd3dbf0b5bcf5999dbdf3ee6b184dfc2e1ca
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-12 11:16:14 -0500

    setting up project

---
+++ src/main/java/com/carey/careytestapp/CareytestappApplication.java
@@ -0,0 +1,13 @@
+package com.carey.careytestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareytestappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareytestappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carey/careytestapp/CareytestappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carey.careytestapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CareytestappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
